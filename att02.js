7131266d41a32c5f07ec8729ee7dcc5614de94c3
Math3Faria <matheusteud12@gmail.com>
2025-04-23 11:17:25 -0300

    Arrumando e adicionando 'MENOR QUE/MAIOR QUE'

--- att02.js
+++ att02.js
@@ -5,24 +5,31 @@ function getRandomInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
 let numAleatorio = getRandomInt(1, 10);
 let tentativaAtual = 1;
 let tentativaMaximo = 3;
 console.log(numAleatorio);
 
 while (tentativaAtual <= tentativaMaximo) {
-    let tentativaNum = parseInt(prompt(`Digite um número de 1 a 10:`));
-
-    if (numAleatorio == tentativaNum) {
+    let tentativaNum = parseInt(prompt(` Digite um número de 1 a 10:`));
+    
+    if (numAleatorio === tentativaNum) {
         alert(`Parabéns! Você acertou o número ${numAleatorio}`);
-        tentativaAtual = tentativaMaximo + 1;
+        break;
     }
     else {
-        alert(`Número ta errado. Tente novamente.`);
+        // Adiciona dica se é maior ou menor
+        if (tentativaNum < numAleatorio) {
+            alert(`Errou! O número é MAIOR que  ${tentativaNum}`);
+        } else {
+            alert(`Errou! O número é MENOR que  ${tentativaNum}`);
+        }
+        
         tentativaAtual++;
-
+        
         if (tentativaAtual > tentativaMaximo) {
-            alert(`Acabaram suas tentativas kkkk era:  ${numAleatorio}.`);
+            alert(`Suas tentativas acabaram! Era o número ${numAleatorio}.`);
         }
     }
 }
